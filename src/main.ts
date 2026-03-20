cde917a49c26ab6e4b465089b730a6ba0d1e0fda
Ramz001 <rkenjaev1@gmail.com>
2026-03-21 04:00:03 +0500

    feat: add gRPC microservice support and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { Transport, type MicroserviceOptions } from "@nestjs/microservices";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: "auth.v1",
+      protoPath: "node_modules/@ramz001-cinama/contracts/proto/auth.proto",
+      url: "localhost:50051",
+      loader: {
+        keepCase: false,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.init();
 }
+
 void bootstrap();
