4c9cee72c7b2ba0ded8cf98df3608cfb72e96d21
Ramz001 <rkenjaev1@gmail.com>
2026-03-23 14:55:30 +0500

    feat(auth): implement authentication module with SendOtp functionality
    - Added AuthModule to the application.
    - Created AuthController with SendOtp method to handle OTP requests.
    - Implemented AuthService as a provider for the AuthModule.

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { GrpcMethod } from "@nestjs/microservices";
+import type {
+  SendOtpRequest,
+  SendOtpResponse,
+} from "@ramz001-cinema/contracts/gen/auth";
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @GrpcMethod("AuthService", "SendOtp")
+  async sendOtp(data: SendOtpRequest): Promise<SendOtpResponse> {
+    console.log("Received SendOtp request:", data);
+    return await Promise.resolve({ ok: true });
+  }
+}
