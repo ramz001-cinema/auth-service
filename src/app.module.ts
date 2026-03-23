193fc2d8f34984a789e7df2dceac7c43e57dc269
Ramz001 <rkenjaev1@gmail.com>
2026-03-23 23:41:19 +0500

    feat: configure gRPC microservice with environment variable for URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { Transport, type MicroserviceOptions } from "@nestjs/microservices";
+import { Transport, MicroserviceOptions } from "@nestjs/microservices";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const config = app.get(ConfigService);
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
       package: "auth.v1",
-      protoPath: "node_modules/@ramz001-cinama/contracts/proto/auth.proto",
-      url: "localhost:50051",
+      protoPath: "node_modules/@ramz001-cinema/contracts/proto/auth.proto",
+      url: config.getOrThrow<string>("AUTH_GRPC_URL"),
       loader: {
         keepCase: false,
         longs: String,
