1ee21793e012c987237439a5ef3298ae862c373c
Ramz001 <rkenjaev1@gmail.com>
2026-03-27 10:42:02 +0500

    feat(auth): implement user creation and OTP handling in AuthService

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,6 +1,7 @@
 import type { PrismaService } from "@/infrastructure/prisma/prisma.service";
 import { Injectable } from "@nestjs/common";
 import type { User } from "@prisma/generated/client";
+import type { UserCreateInput } from "@prisma/generated/models";
 
 @Injectable()
 export class AuthRepository {
@@ -15,6 +16,12 @@ export class AuthRepository {
   async findByEmail(email: string): Promise<User | null> {
     return await this.prismaService.user.findUnique({
       where: { email },
+    });
+  }
+
+  async createUser(data: UserCreateInput): Promise<User> {
+    return await this.prismaService.user.create({
+      data,
     });
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,35 @@
 import { Injectable } from "@nestjs/common";
-import type { SendOtpRequest } from "@ramz001-cinema/contracts/gen/auth";
+import {
+  OtpType,
+  type SendOtpRequest,
+} from "@ramz001-cinema/contracts/gen/auth";
+import type { AuthRepository } from "./auth.repository";
+import type { User } from "@prisma/generated/client";
 
 @Injectable()
 export class AuthService {
-  async sendOtp(data: SendOtpRequest) {}
+  constructor(private readonly authRepository: AuthRepository) {}
+
+  async sendOtp(data: SendOtpRequest) {
+    const { id, type } = data;
+
+    let user: User | null = null;
+
+    switch (type) {
+      case OtpType.OTP_TYPE_PHONE:
+        user = await this.authRepository.findByPhone(id);
+        break;
+      case OtpType.OTP_TYPE_EMAIL:
+        user = await this.authRepository.findByEmail(id);
+        break;
+    }
+
+    if (!user) {
+      await this.authRepository.createUser({
+        email: type === OtpType.OTP_TYPE_EMAIL ? id : undefined,
+        phone: type === OtpType.OTP_TYPE_PHONE ? id : undefined,
+      });
+    }
+    return { ok: true };
+  }
 }
