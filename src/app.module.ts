d4f1e6f9afa73016123d55ac36219a79d303e156
Ramz001 <rkenjaev1@gmail.com>
2026-03-28 17:24:24 +0500

    feat(auth): add Redis and OTP modules with corresponding services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "./infrastructure/prisma/prisma.module";
+import { RedisModule } from "./infrastructure/redis/redis.module";
+import { OtpModule } from './modules/otp/otp.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    RedisModule,
+    OtpModule,
+  ],
 })
 export class AppModule {}
