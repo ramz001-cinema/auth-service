b2312313a653af55c1c0d4aa6d7579d733c378b3
Ramz001 <rkenjaev1@gmail.com>
2026-07-31 22:59:20 +0500

    feat(telegram): add TelegramService with bot configuration

--- src/modules/telegram/telegram.service.ts
+++ src/modules/telegram/telegram.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { EnvType } from '@/common/config'
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
-export class TelegramService {}
+export class TelegramService {
+	private readonly BOT_ID: string
+	private readonly BOT_TOKEN: string
+	private readonly BOT_USERNAME: string
+	private readonly REDIRECT_ORIGIN: string
+
+	constructor(private readonly configService: ConfigService<EnvType>) {
+		this.BOT_ID = this.configService.get('TELEGRAM_BOT_ID') || ''
+		this.BOT_TOKEN = this.configService.get('TELEGRAM_BOT_TOKEN') || ''
+		this.BOT_USERNAME =
+			this.configService.get('TELEGRAM_BOT_USERNAME') || ''
+		this.REDIRECT_ORIGIN =
+			this.configService.get('TELEGRAM_REDIRECT_ORIGIN') || ''
+	}
+}
